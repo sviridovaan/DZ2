8c59f0f5d84d959fc8126508f0d422735a0ab14f
Anna <sviridova_anya@mail.ru>
2022-11-24 21:06:52 +0300

    программа которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите число натуральное число N: ");
+int numberN = int.Parse(Console.ReadLine());
+int count=1;
+while (count<=numberN)
+{
+    if (count%2==0)
+    {
+        Console.Write(count);
+        count++;
+    }
+    else 
+    {
+        Console.Write(" ");
+        count++;
+    }
+}
+
+
